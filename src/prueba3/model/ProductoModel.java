5f19ef42f0a07c71abfd63bd16fb6a9a64674583
vidal7226 <vidalalvarezjosefrancisco@gmail.com>
2023-06-29 10:00:21 -0500

    se implemento la clase metodo producto model

--- src/prueba3/model/ProductoModel.java
+++ src/prueba3/model/ProductoModel.java
@@ -10,4 +10,57 @@ package prueba3.model;
  */
 public class ProductoModel {
     
+    private String nombre;
+    private double precio;
+    private Integer cantidad;
+    private String descripcion;
+
+    public ProductoModel() {
+    }
+
+    public ProductoModel(String nombre, double precio, Integer cantidad, String descripcion) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductoModel{" + "nombre=" + nombre + ", precio=" + precio + ", cantidad=" + cantidad + ", descripcion=" + descripcion + '}';
+    }
+    
+    
 }
